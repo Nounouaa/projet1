c7d67c845e93f5d22d2a3b4df5b325d16ef59f76
LOVAMAMY <lovamamy61@gmail.com>
2025-03-01 21:24:42 +0100

    Sauvegarde des modifications avant pull

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule, provideClientHydration, withEventReplay } from '@angular
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProduitsComponent } from './Controller/produits/produits.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProduitsComponent
   ],
   imports: [
     BrowserModule,
