afb9eebd06123293180d2ef81b46aa558a98f801
LOVAMAMY <lovamamy61@gmail.com>
2025-03-01 21:59:59 +0100

    tus

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Controller/home/home.component';
+import { ProduitsComponent } from './Controller/produits/produits.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component : HomeComponent},
+  {path:"produits",component : ProduitsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProduitsComponent } from './Controller/produits/produits.component';
+import path from 'node:path';
+import { HomeComponent } from './Controller/home/home.component';
+import { RouterModule } from '@angular/router';
+
+const routes = [
+  {path:"", Component:HomeComponent},
+  {path:"produits", Component:ProduitsComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProduitsComponent
+    ProduitsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [
     provideClientHydration(withEventReplay())
